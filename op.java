a78561f760e5ca244bbc8e8c970c369f0a032489
Aashitha Prabhu B <aashitha.ipad@gmail.com>
2026-01-22 11:30:52 +0530

    Add files via upload

---
+++ op.java
@@ -0,0 +1,21 @@
+package javatraining;
+public class op {
+    public static void main(String[] args) {
+        //int x = 50 ,y = 12;
+        
+        // String res = x<y ? "hi" : "hello"; 
+        // System.out.println(res); 
+
+        // int a=20, b=-10;
+        // int c=a + ++b;
+        // int d=a++ - b++ + ++c;
+        // System.out.println(" "+a+" "+b+" "+c+" "+d);
+
+        
+         int a=20, b=-10;
+         int c=a + --b;
+         int d=a-- - b-- + --c;
+         System.out.println(" "+a+" "+b+" "+c+" "+d);
+    }
+
+}

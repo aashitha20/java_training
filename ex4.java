51f7829bd13032fe82f4c0267c4640c7250bb485
Aashitha Prabhu B <aashitha.ipad@gmail.com>
2026-01-22 11:25:51 +0530

    Add files via upload

---
+++ ex4.java
@@ -0,0 +1,36 @@
+package javatraining;
+
+// public class ex1 {
+//     int a=9;
+//     public static void main(String[] args) {
+//         int a=10;
+//         System.out.println(a);
+//     }
+// }
+
+// public class ex2 {
+//     public static void main(String[] args) {
+//         int a;
+//     }
+//     {
+//         a=10;
+//     }
+// }
+
+// public class ex3 {
+//     public static void main(String[] args) {
+//         int a;
+//         {
+//             a=10;
+//             System.out.println(a);
+//         }
+//     }
+// }
+
+public class ex4 {
+    int a=9;
+    {        
+        a=10;
+    }
+    System.out.println(a);
+}

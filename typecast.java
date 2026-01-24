4d1c35e837dcc59a353539d7dda924ff48a2a59d
Aashitha Prabhu B <aashitha.ipad@gmail.com>
2026-01-24 09:09:03 +0530

    Add files via upload

---
+++ typecast.java
@@ -0,0 +1,14 @@
+// narrowing typecasting
+package javatraining;
+public class typecast {
+    public static void main(String[] args) {
+        // narrowing typecasting
+        float f=10.5F;
+        int a=(int)f;
+        System.out.println(a);  
+
+        double d=20.5D;
+        int b=(int)d;
+        System.out.println(b);  
+    }
+}
